2c2e2c35bbcfefb58f9fc1ac713337bf36c93713
Victor Tatai <vtatai@fitbit.com>
2018-04-03 16:21:03 -0700

    Fixing issue when using alternate port number

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -20,15 +20,18 @@ if [ ! -z "$ADVERTISED_HOST" ]; then
     if grep -q "^advertised.host.name" $KAFKA_HOME/config/server.properties; then
         sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
     else
-        echo "advertised.host.name=$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
+        echo "\n" >> $KAFKA_HOME/config/server.properties
+        echo "\nadvertised.host.name=$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
     fi
 fi
 if [ ! -z "$ADVERTISED_PORT" ]; then
     echo "advertised port: $ADVERTISED_PORT"
     if grep -q "^advertised.port" $KAFKA_HOME/config/server.properties; then
         sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+        sed -r -i "s/#(port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
     else
-        echo "advertised.port=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
+        echo "\nadvertised.port=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
+        echo "\nport=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
     fi
 fi
 
